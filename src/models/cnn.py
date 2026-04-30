85259b2ec070030fd325bad1ca76c47cb8dcfff9
sethbfey <sethbfey@gmail.com>
2026-04-30 15:17:15 -0500

    Remove BatchNorm from CIFAR-10 CNN and replace FC head with GAP

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -27,19 +27,14 @@ class CIFAR10_CNN(nn.Module):
     def __init__(self):
         super(CIFAR10_CNN, self).__init__()
         self.conv1 = nn.Conv2d(3,  32, 3, padding=1)
-        self.bn1   = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.bn2   = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn3   = nn.BatchNorm2d(64)
         self.pool  = nn.MaxPool2d(2, 2)
-        self.fc1   = nn.Linear(64 * 8 * 8, 128)
-        self.fc2   = nn.Linear(128, 10)
+        self.fc    = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.bn1(self.conv1(x)))) # -> 16 x 16 x 32
-        x = self.pool(F.relu(self.bn2(self.conv2(x)))) # -> 8 x 8 x 64
-        x = F.relu(self.bn3(self.conv3(x)))            # -> 8 x 8 x 64
-        x = x.view(-1, 64 * 8 * 8)                     # -> 4096
-        x = F.relu(self.fc1(x))                        # -> 128
-        return self.fc2(x)                             # -> 10
+        x = self.pool(F.relu(self.conv1(x))) # -> 16 x 16 x 32
+        x = self.pool(F.relu(self.conv2(x))) # -> 8 x 8 x 64
+        x = F.relu(self.conv3(x))            # -> 8 x 8 x 64
+        x = x.mean(dim=(2, 3))               # global avg pool -> 64
+        return self.fc(x)                    # -> 10
